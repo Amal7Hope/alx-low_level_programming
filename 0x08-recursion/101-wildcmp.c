489626599f212e249195c0c4ad57bce6324c1a5e
Amal7Hope <elmouchiramal@gmail.com>
2023-04-05 14:54:10 -0700

    pls

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * wildcmp - compares two strings
+ * @str1: ponter string 1
+ * @str2: pointer string 2
+ * Return: 0
+ */
+int wildcmp(char *str1, char *str2)
+{
+	if (*str1 == '\0')
+	{
+		if (*str2 != '\0' && *str2 == '*')
+		{
+			return (wildcmp(str1, str2 + 1));
+		}
+		return (*str2 == '\0');
+	
+	}
+	if (*str2 == '*')
+	{
+		return (wildcmp(str1 + 1, str2) || wildcmp(str1, str2 + 1));
+	}
+	else if (*str1 == *str2)
+	{
+		return (wildcmp(str1 + 1, str2 + 1));
+	}
+	return (0);
+}
