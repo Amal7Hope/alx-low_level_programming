a9fbaed5f918abe0da1c9ff815b9461aef0433ec
Amal7Hope <elmouchiramal@gmail.com>
2023-04-08 17:04:38 -0700

    test

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - writr a program that pronts the number of argument
+ * @argc: this is the argument count
+ * @argv: this is the argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void)argv;
+	return (0);
+
+}
