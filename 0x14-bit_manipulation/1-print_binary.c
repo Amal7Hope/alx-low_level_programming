208f6687e9429d15f77e0f19b17ea78870b076ea
Amal7Hope <elmouchiramal@gmail.com>
2023-07-06 07:33:52 -0700

    binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_binary - print string ( number as binary s)
+ * @n: T number
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	int bit = sizeof(n) * 8, p = 0;
+
+	while (bit)
+	{
+		if (n & 1L << --bit)
+		{
+			_putchar('1');
+			p++;
+		}
+		else if (p)
+			_putchar('0');
+	}
+	if (!p)
+		_putchar('0');
+}
